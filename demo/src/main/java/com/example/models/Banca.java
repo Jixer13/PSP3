99ce14607a76201714c0bcdf41d666854559bce8
CarlosA2006 <carlosalvarezguillen222@gmail.com>
2025-12-16 10:55:09 +0100

    Cambiados tiempos en banca y jugadores

--- demo/src/main/java/com/example/models/Banca.java
+++ demo/src/main/java/com/example/models/Banca.java
@@ -55,7 +55,7 @@ public class Banca {
         while (estadoBanca) {
             try {
                 // Esperamos 5 segundos
-                Thread.sleep(5000);
+                Thread.sleep(2000);
 
             } catch (InterruptedException eHilo) {
                 Thread.currentThread().interrupt();
@@ -65,7 +65,7 @@ public class Banca {
             // GENERACIÓN DE NÚMERO GANADOR EN EL HILO BANCA //
             numeroApostado = (int) (Math.random() * 37); // 0 al 36
 
-            System.out.println("\n🏦 [ BANCA ] Ha generado el número " + numeroApostado);
+
             EstadoJuego.botsApostando = false;
 
             // NOTIFICAMOS que la ronda de generación ha terminado
@@ -75,7 +75,7 @@ public class Banca {
 
             try {
                 // Esperamos 35 segundos antes del siguiente ciclo
-                Thread.sleep(35000);
+                Thread.sleep(8000);
 
             } catch (InterruptedException eHilo) {
                 Thread.currentThread().interrupt();
